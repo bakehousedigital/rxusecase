e783e37a6ec37b4f53010dc679e01ae37a1a90ea
Roman Reaboi <roman.reaboi@gmail.com>
2019-03-24 10:38:10 +0200

    Complete comments for Response and ResponseConsumer classes

--- rxusecase/src/main/java/digital/bakehouse/rxusecase/Response.java
+++ rxusecase/src/main/java/digital/bakehouse/rxusecase/Response.java
@@ -2,10 +2,23 @@ package digital.bakehouse.rxusecase;
 
 import digital.bakehouse.rxusecase.toolbox.Objects;
 
+/**
+ * Wrapper of success and failure data.
+ * Instances of this class are emitted by use-cases as outputs
+ * for the action/logic/mechanism they wrap/define.
+ *
+ * @param <O> Output type
+ */
 public class Response<O> {
     private O data;
     private Failure failure;
 
+    /**
+     * Wrap success data and failure object
+     *
+     * @param data    Success
+     * @param failure Failure
+     */
     public Response(O data, Failure failure) {
         this.data = data;
         this.failure = failure;
@@ -19,26 +32,68 @@ public class Response<O> {
         return failure;
     }
 
+    /**
+     * Verify whether this {@link Response} is a wrapper of
+     * success or failure data.
+     *
+     * @return true if success holder, false if failure holder
+     */
     public boolean isSuccessful() {
         return failure == null;
     }
 
+    /**
+     * Factory method to wrap success data into a {@link Response} object.
+     *
+     * @param output Success data
+     * @param <O>    Output type
+     * @return Response object
+     */
     public static <O> Response<O> succeed(O output) {
         return new Response<>(output, null);
     }
 
+    /**
+     * Factory method to wrap a failure into a {@link Response} object.
+     *
+     * @param failure Failure data
+     * @param <O>     Output type
+     * @return Response object
+     */
     public static <O> Response<O> fail(Failure failure) {
         return new Response<>(null, failure);
     }
 
+    /**
+     * Factory method to wrap the passed code and message into
+     * a failure {@link Response} object.
+     *
+     * @param errorCode    Failure code
+     * @param errorMessage Failure message
+     * @param <O>          Output type
+     * @return Response object
+     */
     public static <O> Response<O> fail(String errorCode, String errorMessage) {
         return fail(new Failure(errorCode, errorMessage));
     }
 
+    /**
+     * Factory method that builds a {@link O} typed failure @link Response} object,
+     * from the {@link Response#failure} of passed input
+     *
+     * @param response Failure data holder
+     * @param <O>      Output type
+     * @return Response object
+     */
     public static <O> Response<O> fail(Response<?> response) {
         return fail(response.failure);
     }
 
+    /**
+     * Factory method to create a {@link Void} wrapped success {@link Response} object.
+     *
+     * @return Response object
+     */
     public static Response<Void> empty() {
         return succeed(null);
     }
--- rxusecase/src/main/java/digital/bakehouse/rxusecase/ResponseConsumer.java
+++ rxusecase/src/main/java/digital/bakehouse/rxusecase/ResponseConsumer.java
@@ -2,16 +2,36 @@ package digital.bakehouse.rxusecase;
 
 import io.reactivex.functions.Consumer;
 
-@SuppressWarnings({"WeakerAccess", "unused"})
+/**
+ * This is a {@link Consumer} implementation for emissions of {@link Response} classes.
+ * It simplifies differentiated handling of response scenarios applying a mechanism
+ * to gracefully consume response success and response failures.
+ * <p>
+ * See {@link Response#isSuccessful()} for reference.
+ *
+ * @param <O> Output type
+ */
 public class ResponseConsumer<O> implements Consumer<Response<O>> {
 
     private Consumer<O> successConsumer;
     private Consumer<Failure> failureConsumer;
 
+    /**
+     * Create a consumer which handles only success cases
+     *
+     * @param successConsumer Delegate consumer
+     */
     public ResponseConsumer(Consumer<O> successConsumer) {
         this.successConsumer = successConsumer;
     }
 
+    /**
+     * Create a consumer to handle success and failure
+     * cases of {@link Response} class
+     *
+     * @param successConsumer Delegate success consumer
+     * @param failureConsumer Delegate failure consumer
+     */
     public ResponseConsumer(Consumer<O> successConsumer,
                             Consumer<Failure> failureConsumer) {
         this.successConsumer = successConsumer;
@@ -31,14 +51,38 @@ public class ResponseConsumer<O> implements Consumer<Response<O>> {
         }
     }
 
+    /**
+     * Factory method for consuming {@link Response} success.
+     * Failure will be ignored.
+     *
+     * @param successConsumer Delegate success consumer
+     * @param <O>             Output type
+     * @return Response consumer
+     */
     public static <O> ResponseConsumer<O> consumeSuccess(Consumer<O> successConsumer) {
         return new ResponseConsumer<>(successConsumer);
     }
 
+    /**
+     * Factory method for consuming {@link Response} failure.
+     * Success will be ignored.
+     *
+     * @param failureConsumer Delegate failure consumer
+     * @param <O>             Output type
+     * @return Response consumer
+     */
     public static <O> ResponseConsumer<O> consumeFailure(Consumer<Failure> failureConsumer) {
         return new ResponseConsumer<>(null, failureConsumer);
     }
 
+    /**
+     * Factory method for consuming {@link Response} success and failures.
+     *
+     * @param successConsumer Delegate success consumer
+     * @param failureConsumer Delegate failure consumer
+     * @param <O>             Output type
+     * @return Response consumer
+     */
     public static <O> ResponseConsumer<O> consume(Consumer<O> successConsumer,
                                                   Consumer<Failure> failureConsumer) {
         return new ResponseConsumer<>(successConsumer, failureConsumer);
