1f2dd4ff761da78d3a7c9381331a5e2ada14bbda
Roman Reaboi <roman.reaboi@gmail.com>
2019-03-24 10:01:53 +0200

    Complete comments for RxUseCase class

--- rxusecase/src/main/java/digital/bakehouse/rxusecase/RxUseCase.java
+++ rxusecase/src/main/java/digital/bakehouse/rxusecase/RxUseCase.java
@@ -10,6 +10,7 @@ import digital.bakehouse.rxusecase.operation.Continuous;
 import digital.bakehouse.rxusecase.operation.DelegateUseCase;
 import digital.bakehouse.rxusecase.operation.RxSource;
 import digital.bakehouse.rxusecase.operation.Synchronous;
+import digital.bakehouse.rxusecase.toolbox.Objects;
 import digital.bakehouse.rxusecase.toolbox.SafeEmitter;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
@@ -131,34 +132,93 @@ public abstract class RxUseCase<I, O> {
     }
 
     /**
+     * Create use-case observable with input.
+     * This method will get called by invoking any of the:
+     * {@link #create()}, {@link #create(Object)}, {@link #create(Request)} methods.
+     * Implement it to define the logic/mechanism/action that this use-case represents.
+     *
      * @param input Use-case input
      * @return Observable stream
      */
     protected abstract Observable<Response<O>> execute(I input);
 
+    /**
+     * Emit the passed object wrapped into a {@link Response}.
+     * Utility method that builds and returns an {@link Observable}
+     * that emits a successful {@link Response} wrapping the output.
+     *
+     * @param output Use-case output
+     * @return Observable stream
+     */
     protected Observable<Response<O>> justSucceed(O output) {
         return just(Response.succeed(output));
     }
 
+    /**
+     * Emit the passed code and message wrapped into a {@link Response}.
+     * Utility method that builds and returns an {@link Observable}
+     * that emits a failure {@link Response} wrapping the passed
+     * error code and error message
+     *
+     * @param code    Error code
+     * @param message Error message
+     * @return Observable stream
+     */
     protected Observable<Response<O>> justFail(String code, String message) {
         return just(Response.fail(code, message));
     }
 
+    /**
+     * Decorate this use-case with a list of {@link UseCaseDecorator} implementations.
+     * Effectively, the use-case observable returned from {@link #execute(Object)} method
+     * will be decorated in a chain manner with items in this list.
+     * The order of elements in the passed list is important because the decorators
+     * may decide to pre and post-process streams, for ex. by filtering out the emission
+     * of certain elements.
+     * NOTE: Invoking this method will disable the global decorators for this instance
+     * of this use-case.
+     * <p>
+     * See {@link UseCaseDecorator} for reference
+     *
+     * @param decorators List of decorators
+     * @param <T>        Type of this use-case
+     * @return This use-case
+     */
     @SuppressWarnings("unchecked")
     public final <T extends RxUseCase<I, O>> T decorateWith(Collection<UseCaseDecorator> decorators) {
+        Objects.requireNonNull(decorators, "Decorators should not be null!");
         createDecorators().addAll(decorators);
         return (T) this;
     }
 
+    /**
+     * Add a {@link UseCaseDecorator} to the list of items decorating this use-case.
+     * NOTE: Invoking this method will disable the global decorators for this instance
+     * of this use-case.
+     * Please see {@link #decorateWith(Collection)} and {@link UseCaseDecorator}
+     * for reference
+     *
+     * @param decorator Decorator of use-case
+     * @param <T>       Type of this use-case
+     * @return This use-case
+     */
     @SuppressWarnings("unchecked")
     public final <T extends RxUseCase<I, O>> T decorateWith(UseCaseDecorator decorator) {
+        Objects.requireNonNull(decorator, "Decorator should not be null!");
         createDecorators().add(decorator);
         return (T) this;
     }
 
+    /**
+     * Remove all the decorators previously set for this use-case including
+     * the global ones.
+     *
+     * @param <T> Type of this use-case
+     * @return This use-case
+     */
     @SuppressWarnings("unchecked")
     public final <T extends RxUseCase<I, O>> T decorateWithNothing() {
-        createDecorators();
+        createDecorators(true);
         return (T) this;
     }
 
@@ -170,7 +230,11 @@ public abstract class RxUseCase<I, O> {
     }
 
     private Collection<UseCaseDecorator> createDecorators() {
-        if (decorators == null) {
+        return createDecorators(false);
+    }
+
+    private Collection<UseCaseDecorator> createDecorators(boolean forceNewInstance) {
+        if (decorators == null || forceNewInstance) {
             decorators = new ArrayList<>();
             addDefaultDecoratorsInto(decorators);
         }
@@ -194,36 +258,120 @@ public abstract class RxUseCase<I, O> {
         decorators.add(FailureExceptionConverter.getDefault());
     }
 
+    /**
+     * Add a global decorator for use-cases.
+     * All use-case observables returned from their respective {@link #execute(Object)} method
+     * will be decorated by this item, unless they define local {@link UseCaseDecorator}
+     * or clear the decoration mechanism, see {@link #decorateWith(Collection)},
+     * {@link #decorateWith(UseCaseDecorator)} and {@link #decorateWithNothing()} for this.
+     *
+     * @param decorator Global decorator
+     */
     public static void addDecorator(UseCaseDecorator decorator) {
+        Objects.requireNonNull(decorator, "Decorator should not be null!");
         GLOBAL_DECORATORS.add(decorator);
     }
 
+    /**
+     * Remove a global decorator of use-cases.
+     *
+     * @param decorator Global decorator
+     */
     public static void removeDecorator(UseCaseDecorator decorator) {
+        Objects.requireNonNull(decorator, "Decorator should not be null!");
         GLOBAL_DECORATORS.remove(decorator);
     }
 
+    /**
+     * Create a use-case from a {@link Synchronous} operation.
+     * This method will wrap the logic / action / mechanism defined
+     * in the operation into an instance of {@link RxUseCase}.
+     * <p>
+     * See {@link Synchronous} for reference.
+     *
+     * @param operation Use-case operation
+     * @param <I>       Input type
+     * @param <O>       Output type
+     * @return Rx use-case
+     */
     public static <I, O> RxUseCase<I, O> fromSynchronous(Synchronous<I, O> operation) {
         return fromSource(input -> toRx(operation, input));
     }
 
+    /**
+     * Create a use-case from a {@link Asynchronous} operation.
+     * This method will wrap the logic / action / mechanism defined
+     * in the operation into an instance of {@link RxUseCase}.
+     * <p>
+     * See {@link Asynchronous} for reference.
+     *
+     * @param operation Use-case operation
+     * @param <I>       Input type
+     * @param <O>       Output type
+     * @return Rx use-case
+     */
     public static <I, O> RxUseCase<I, O> fromAsynchronous(Asynchronous<I, O> operation) {
         return fromSource(input -> toRx(operation, input));
     }
 
+    /**
+     * Create a use-case from a {@link Continuous} operation.
+     * This method will wrap the logic / action / mechanism defined
+     * in the operation into an instance of {@link RxUseCase}.
+     * <p>
+     * See {@link Continuous} for reference.
+     *
+     * @param operation Use-case operation
+     * @param <I>       Input type
+     * @param <O>       Output type
+     * @return Rx use-case
+     */
     public static <I, O> RxUseCase<I, O> fromContinuous(Continuous<I, O> operation) {
         return fromSource(input -> toRx(operation, input));
     }
 
+    /**
+     * Create a use-case from an implementation of {@link RxSource}.
+     * This method will wrap the logic / action / mechanism defined
+     * in the source into an instance of {@link RxUseCase}.
+     * It is useful for situations when extending {@link RxUseCase} class
+     * is not an option, and the other operations are not suitable.
+     * <p>
+     * See {@link RxSource} for reference.
+     *
+     * @param source Observable source
+     * @param <I>    Input type
+     * @param <O>    Output type
+     * @return Rx use-case
+     */
     public static <I, O> RxUseCase<I, O> fromSource(RxSource<I, O> source) {
         return new DelegateUseCase<>(source);
     }
 
+    /**
+     * Transform a {@link Synchronous} operation into an {@link Observable}.
+     *
+     * @param operation Use-case operation
+     * @param input     Operation input
+     * @param <I>       Input type
+     * @param <O>       Output type
+     * @return Observable stream
+     */
     protected static <I, O> Observable<Response<O>> toRx(Synchronous<I, O> operation,
                                                          I input) {
         return fromCallable(() -> operation.act(input))
                 .map(Response::succeed);
     }
 
+    /**
+     * Transform an {@link Asynchronous} operation into an {@link Observable}.
+     *
+     * @param operation Use-case operation
+     * @param input     Operation input
+     * @param <I>       Input type
+     * @param <O>       Output type
+     * @return Observable stream
+     */
     protected static <I, O> Observable<Response<O>> toRx(Asynchronous<I, O> operation,
                                                          I input) {
         return safeCreate(emitter ->
@@ -242,6 +390,15 @@ public abstract class RxUseCase<I, O> {
                 }));
     }
 
+    /**
+     * Transform a {@link Continuous} operation into an {@link Observable}.
+     *
+     * @param operation Use-case operation
+     * @param input     Operation input
+     * @param <I>       Input type
+     * @param <O>       Output type
+     * @return Observable stream
+     */
     protected static <I, O> Observable<Response<O>> toRx(Continuous<I, O> operation,
                                                          I input) {
         return safeCreate((ObservableEmitter<Response<O>> emitter) ->
@@ -265,6 +422,18 @@ public abstract class RxUseCase<I, O> {
                 .doOnDispose(() -> operation.cancel(input));
     }
 
+    /**
+     * Create an {@link Observable} using a {@link SafeEmitter}.
+     * This will allow invocations of {@link ObservableEmitter#onNext(Object)},
+     * {@link ObservableEmitter#onNext(Object)} and {@link ObservableEmitter#onNext(Object)}
+     * without first verifying the {@link ObservableEmitter#isDisposed()} status.
+     * <p>
+     * See {@link SafeEmitter} for reference.
+     *
+     * @param subscriber OnSubscribe hook
+     * @param <O>        Output type
+     * @return Observable stream
+     */
     protected static <O> Observable<Response<O>> safeCreate(
             ObservableOnSubscribe<Response<O>> subscriber) {
         return Observable.create(emitter ->
--- rxusecase/src/main/java/digital/bakehouse/rxusecase/toolbox/Objects.java
+++ rxusecase/src/main/java/digital/bakehouse/rxusecase/toolbox/Objects.java
@@ -18,4 +18,12 @@ public final class Objects {
     public static int hash(Object... var0) {
         return Arrays.hashCode(var0);
     }
+
+    public static <T> T requireNonNull(T var0, String var1) {
+        if (var0 == null) {
+            throw new NullPointerException(var1);
+        } else {
+            return var0;
+        }
+    }
 }
